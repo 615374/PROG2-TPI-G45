c24c0e769c304282f73ac17be728ddcf8b12b0c1
Sol Lezcano <sollezcanoutn@gmail.com>
2026-06-03 08:58:40 -0300

    Implementacion clase Profesional

---
+++ Profesional.h
@@ -0,0 +1,42 @@
+#pragma once
+
+class Profesional{
+    private:
+        int idProfesional;
+        char nombre[50];
+        char especialidad[50];
+        float porcentajeComision;
+        bool estado; // baja lógica
+
+        // Método privado auxiliar para calcular el ID autoincremental
+        int generarNuevoId();
+
+    public:
+        // Constructor
+        Profesional();
+
+        // Métodos principales
+        void cargar();
+        void mostrar();
+
+        //Método para validar Id
+        bool buscarPorId(int id);
+
+        // Setters
+        void setIdProfesional(int id);
+        void setNombre(const char* n);
+        void setEspecialidad(const char* e);
+        void setComision(float c);
+        void setEstado(bool e);
+
+        //Getters
+        int getIdProfesional();
+        const char* getNombre();
+        const char* getEspecialidad();
+        float getComision();
+        bool getEstado();
+
+        // Métodos de persistencia binaria (.dat)
+        bool leerDisco(int pos);
+        bool escribirDisco();
+};
