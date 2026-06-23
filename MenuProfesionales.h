75b5a1f24812c0b3d6bf0ff3e61d2f2505a7e852
Sol Lezcano <sollezcanoutn@gmail.com>
2026-06-23 15:49:40 -0300

    Actualiza menu profesionales y profesional

--- MenuProfesionales.h
+++ MenuProfesionales.h
@@ -2,4 +2,15 @@
 
 void menuProfesionales();
 
+//DECLARACIONES DE FUNCIONES GLOBALES
+
+// Mantenimiento
+void modificarProfesional();
+bool darDeBajaProfesional();
+
+// Listados Masivos
+void listarProfesionalesInactivos();
+void listarProfesionalesPorEspecialidad();
+void listarProfesionalesPorVolumenServicios();
+void liquidacionComisiones();
 
